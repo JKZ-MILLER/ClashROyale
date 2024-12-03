2613234891c757a529f4bd76d00d5a6759181696
JKZ-MILLER <juancarlosdasilva2021@gmail.com>
2024-12-02 22:06:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mundo',
+    'O que é o Arena de Clash Royale?',
+    'A Arena é o campo de batalha onde jogadores enfrentam seus oponentes em duelos. Existem diferentes Arenas, cada uma com características únicas e desafios.'
+);
+
+criaCartao(
+    'Cartas',
+    'O que são as cartas no Clash Royale?',
+    'As cartas são as unidades e feitiços que os jogadores utilizam durante as batalhas. Cada carta tem habilidades e atributos específicos, como dano, saúde e custo de Elixir.'
+);
+
+criaCartao(
+    'Personagens',
+    'Quem é o Cavaleiro?',
+    'O Cavaleiro é uma das cartas mais populares de Clash Royale. Ele é uma unidade terrestre que causa dano significativo e é muito útil para defesa e ataque.'
+);
+
+criaCartao(
+    'Estratégias',
+    'O que é um Push no Clash Royale?',
+    'Um Push é uma estratégia de ataque coordenada em que o jogador envia várias unidades simultaneamente em direção à torre inimiga para causar o maior dano possível.'
+);
